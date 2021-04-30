041b814127a4837211978618fc471be5bb05a0e2
CyrilGendarme <cyril.gendarme@student.hepl.be>
2021-04-30 17:28:22 +0200

    reupload partie web

---
+++ TP5_progra_reseaux/src/autres/BeanAccessOracle.java
@@ -0,0 +1,57 @@
+package autres;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+
+public class BeanAccessOracle {
+	
+	
+
+	Class leDriver;
+	Connection con;
+	Statement instruc;
+	
+	public BeanAccessOracle(String username) throws ClassNotFoundException, SQLException, Exception, IllegalAccessException {
+		
+		leDriver = Class.forName("oracle.jdbc.driver.OracleDriver");								
+		
+		String url  = "jdbc:oracle:thin:@localhost:1521/orcl";						
+	    //String username = "CC";
+	    String pwd = "gendarme";
+	    System.out.println("!!!!!!!!!!!!!! " + url + " " + username + " " + pwd);
+	    
+		con = DriverManager.getConnection(url, username, pwd);   	
+		instruc = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);  
+	}
+	
+	
+	public ResultSet executeQuery(String s) throws SQLException {
+		ResultSet rs = instruc.executeQuery(s); 
+		return rs;
+	}
+	
+	
+	public String executeProcedureChangeStock(int quant, String item_name) throws SQLException {
+		
+		CallableStatement cs = con.prepareCall("{CALL MERCHANT.ChangeStock(?,?,?)}");
+		cs.setInt(1, quant);
+		cs.setString(2, item_name);
+		cs.registerOutParameter(3, Types.VARCHAR);
+		cs.executeUpdate();
+		String rep =  cs.getString(3); System.out.println("Rep de la procédure dans bean : " + rep);
+		return rep;
+	}
+	
+	public int executeUpdate(String s) throws SQLException {
+		int rs = instruc.executeUpdate(s); 
+		return rs;
+	}
+	
+	
+
+}
